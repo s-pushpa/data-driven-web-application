fcbd86c3d99fba64fef7da1369e74fd7986f9a6e
s-pushpa <133870005+s-pushpa@users.noreply.github.com>
2023-05-17 19:16:18 +0000

    details

--- app.py
+++ app.py
@@ -37,14 +37,23 @@ def login():
     
     return render_template('login.html')
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Get the form data
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Perform validation and account creation logic here
+        # For example, check if the username is available and create a new user account
+        
+        # Redirect to the home page after successful signup
+        return redirect('/')
+    return render_template('signup.html')
 
-
-
-
-
-
-
-
+@app.route('/about')
+def about():
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
